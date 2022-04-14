d712912f7e274c9eca6ddb3726125cbc13e40367
Vortlan <utkumacit.55@gmail.com>
2022-04-14 17:02:48 -0500

    Model and Dynamic Contents

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const path = require('path');
 
+const Photo = require('./models/Photo');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 //Template engine
 app.set('view engine', 'ejs');
@@ -13,9 +19,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
@@ -23,8 +33,8 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req, res) => {
-  console.log(req.body);
+app.post('/photos', async (req, res) => {
+  Photo.create(req.body);
   res.redirect('/');
 });
 
