7d4b773ac648ec749f1bbab0676645fcc4922a3d
Vortlan <utkumacit.55@gmail.com>
2022-04-15 17:48:52 -0500

    Photo CRUD Added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
 
 const ejs = require('ejs');
 const path = require('path');
-
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
@@ -17,18 +18,17 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 //Routes
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-DateCreated');
   res.render('index', {
     photos,
   });
 });
 
 app.get('/photos/:id', async (req, res) => {
-  //console.log(req.params.id);
-  // res.render('about');
   const photo = await Photo.findById(req.params.id);
   res.render('photo', {
     photo,
@@ -43,8 +43,30 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  Photo.create(req.body);
-  res.redirect('/');
+  // console.log(req.files.image);
+  //Photo.create(req.body);
+  //res.redirect('/');
+
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(
+    uploadPath,
+
+    async () => {
+      Photo.create({
+        ...req.body,
+        image: '/uploads/' + uploadeImage.name,
+      });
+      res.redirect('/');
+    }
+  );
 });
 
 const port = 3000;
